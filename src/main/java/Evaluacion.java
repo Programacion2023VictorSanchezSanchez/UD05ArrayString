b8c42b32ca4871c9bcb60c53b0a9985453f77c4c
vagosu <victorss_26@hotmail.com>
2023-12-15 16:52:54 +0100

    Evaluacion a medias

--- src/main/java/Evaluacion.java
+++ src/main/java/Evaluacion.java
@@ -50,14 +50,142 @@ public class Evaluacion {
 
     }
 
+    //Sobrecargamos el to string
     public String toString(){
+        String s = "";
+        String nombreAsignatura = "Asignatura: " + this.nombreAsignatura + "\n";
         if(this.listaNotas != null){
-            return this.nombreAsignatura + " \n";
+            for(int i = 0 ; i < this.listaNotas.length ; i++){
+                System.out.println(this.nombreAsignatura);
+            s =  "Alumno " + i+1 + ": " + this.listaNotas[i] + "\n";
+
+
+            }
         }
         else{
-            return this.nombreAsignatura + "\n" +
-            "Sin notas por el momento";
+            s = "Sin notas por el momento";
         }
+        s = nombreAsignatura + s;
+        return s;
+    }
+
+    //Metodo para la media del array
+    public double media(){
+        double media = -1;
+        double suma = 0;
+        if(this.listaNotas != null) {
+            for (int i = 0; i < this.listaNotas.length; i++) {
+                suma = suma + this.listaNotas[i];
+            }
+            media = suma / this.listaNotas.length;
+        }
+
+        return media;
+    }
+
+    //Metodos para el minimo y el maximo del array
+    public double minimo(){
+        double minimo = 11;
+        if(this.listaNotas != null) {
+            for (int i = 0; i < this.listaNotas.length; i++) {
+                if(this.listaNotas[i] < minimo){
+                    minimo = this.listaNotas[i];
+                }
+            }
+
+        } else {
+            minimo = -1;
+        }
+
+        return minimo;
+    }
+    public double maximo(){
+        double maximo = 0;
+        if(this.listaNotas != null) {
+            for (int i = 0; i < this.listaNotas.length; i++) {
+                if(this.listaNotas[i] > maximo){
+                    maximo = this.listaNotas[i];
+                }
+            }
+
+        } else {
+            maximo = -1;
+        }
+
+        return maximo;
+    }
+
+    //Metodos para el total de suspensos y aprobados
+    public int totalSuspensos(){
+        int suspensos = 0;
+        if(this.listaNotas != null) {
+            for (int i = 0; i < this.listaNotas.length; i++) {
+                if(this.listaNotas[i] < 5){
+                    suspensos++;
+                }
+            }
+
+        } else {
+            suspensos = -1;
+        }
+
+        return suspensos;
+    }
+    public int totalAprobados(){
+        int aprobados = 0;
+        if(this.listaNotas != null) {
+            for (int i = 0; i < this.listaNotas.length; i++) {
+                if(this.listaNotas[i] >= 5){
+                    aprobados++;
+                }
+            }
+
+        } else {
+            aprobados = -1;
+        }
+
+        return aprobados;
+    }
+
+    //Metodo para cambiar la nota de un alumno
+    public void cambiarNota(double nota, int alumno){
+        if(alumno < this.listaNotas.length-1){
+            this.listaNotas[alumno] = nota;
+        }
+    }
+
+    //Metodos para mostrar el mejor y el peor alumno
+    public int mejorAlumno(){
+        int indiceMaximo = 0;
+        double maximo = 0;
+        for(int i = 0 ; i < this.listaNotas.length ; i++){
+            if(this.listaNotas[i] > maximo){
+                indiceMaximo = i;
+            }
+        }
+        return indiceMaximo;
+    }
+    public int peorAlumno(){
+        int indiceMinimo = 0;
+        double minimo = 11;
+        for(int i = 0 ; i < this.listaNotas.length ; i++){
+            if(this.listaNotas[i] < minimo){
+                indiceMinimo = i;
+            }
+        }
+        return indiceMinimo;
+    }
+
+    //Metodo para mostrar la nota de un alumno
+    public double notaAlumno(int indice){
+        double nota = 0;
+        if(indice < this.listaNotas.length-1){
+            nota = this.listaNotas[indice];
+        }
+        else{
+            nota = -1;
+        }
+        return nota;
     }
 }
 
